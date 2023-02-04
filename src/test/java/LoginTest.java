27511db1d8ca00da6d33f9bbaaa523741432548c
rohit <ansh.sds@gmail.com>
2023-02-04 23:09:07 +0530

    This is first commmit in new project

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	
+	
+	public static WebDriver driver;
+	@BeforeSuite
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		
+		driver = new ChromeDriver();
+		
+		
+	}
+	@Test
+	public void doLogin() throws InterruptedException
+	{
+		driver.get("https://www.gmail.com");
+		
+		driver.findElement(By.name("identifier")).sendKeys("ansh.sds@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span")).click();
+		Thread.sleep(3000);
+	}
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
